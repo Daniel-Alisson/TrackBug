7ae38478bfbbbf45d3e4d2663c27177d10db89f1
Daniel Alisson <130872709+Daniel-Alisson@users.noreply.github.com>
2025-01-13 17:34:19 -0300

    V1.5
    Organizando as pastas

--- TrackBug/src/main/java/com/sistema/trackbug/colaborador/Servicos.java
+++
@@ -1,9 +0,0 @@
-package com.sistema.trackbug.colaborador;
-
-public interface Servicos {
-    void cadastrarColaborador(Colaborador colaborador);
-    void deslogarColaborador();
-    static boolean loginColaborador(String email, String senha) {
-        return false;
-    }
-}
